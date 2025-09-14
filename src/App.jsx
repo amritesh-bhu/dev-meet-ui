7148138fb30b51188407256bc5dccc4a3307d27a
amritesh-bhu <amriteshbhu438@gmail.com>
2025-09-14 15:09:00 +0530

    configured initial set up of the routing!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Body } from "./body"
+import { BrowserRouter, Route, Routes } from "react-router"
+import { LogIn } from "./login"
+import { Userprofile } from "./user-profile"
+
 export const App = () => {
 
   return (
     <>
-      <div>Hello World</div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/user-profile" element={<Userprofile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
