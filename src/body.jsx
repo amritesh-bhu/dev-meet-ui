6c7c7ea160c05f9cba49a6fbc445d7f84018e8eb
amritesh-bhu <amriteshbhu438@gmail.com>
2025-09-27 21:24:20 +0530

    added navbar and outlet for children component in the body!!

--- src/body.jsx
+++ src/body.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router"
+import { NavBar } from "./nav-bar"
+
 export const Body = () => {
     return (
-        <div>
-            Body
+        <div className="bg-base-100">
+            <NavBar />
+            <Outlet />
+            {/* <Footer /> */}
         </div>
     )
 }
